a528f2040548c93f3d5d27e8d25e77e6dc752c9b
zubairmohammedaziz-dotcom <zubair.mohammed.aziz@gmail.com>
2026-07-20 00:15:22 +0000

    Fix Google Analytics

--- resume-roast-ai/app/layout.tsx
+++ resume-roast-ai/app/layout.tsx
@@ -37,23 +37,21 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
+      <body className="min-h-full flex flex-col">
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-1NY8X9F191"
           strategy="afterInteractive"
         />
+
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-1NY8X9F191');
           `}
         </Script>
-      </head>
 
-      <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
       </body>
     </html>
